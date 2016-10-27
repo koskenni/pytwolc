33d7016c23f3a9353f7b9b24c7de494590e2ff4e
Kimmo Koskenniemi <koskenni@gmail.com>
2016-10-27 19:10:05 +0300

    slightly improved documentation

--- twbt.py
+++ twbt.py
@@ -1,12 +1,34 @@
+"""Module for detailed handling transducers
+"""
 import hfst
 
 def pairname(insym, outsym):
+    """Convert a pair of symbols into a single label
+
+insym -- input symbol as a string
+outsym -- output symbol as a string
+
+Returns a string notation of the pair, eg.
+
+>>> pairname ('a', 'a')
+a
+>>> pairname('i','j')
+i:j
+
+"""
     if insym == outsym:
         return(insym)
     else:
         return(insym + ":" + outsym)
 
 def equivpairs(bfst):
+    """Find and print all sets of equivalent transition pairs.
+
+bfst -- a HfstBasicTransducer whose transition symbol pairs are analyzed
+
+Sets of transition symbol pairs behaving identicaly are computed.
+The sets are printed if they contain more than one element.
+"""
     transitions_for_pairsymbol = {} # {pairsym: list of trs, ..}
     for state in bfst.states():
         for arc in bfst.transitions(state):
@@ -34,6 +56,23 @@ def equivpairs(bfst):
     return(labelsym, pairsymbols_for_transition_sets)
 
 def printfst(fst, print_equiv_classes):
+    """Pretty-prints a HfstTransducer or a HfstBasicTransducer.
+
+fst -- the transducer to be pretty-printed
+print_equiv_classes -- if True, then print also
+                       the equivalence classes
+
+If the transducer has a name, it is printed as a heading.
+
+>>> twbt.printfst(hfst.regex("a* [b:p|c] [c|b:p]"), True)
+
+  0 . -> 0  a ; -> 1  b:p ; 
+  1 . -> 2  b ; 
+  2 : 
+Classes of equivalent symbols:
+  b:p c
+
+"""
     name = fst.get_name()
     print(name)
     bfst = hfst.HfstBasicTransducer(fst)
