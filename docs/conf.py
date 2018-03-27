4f32882654c9ffba850d86fa51ad0830ca4ec36b
Kimmo Koskenniemi <kimmo.koskenniemi@helsinki.fi>
2018-03-27 10:27:14 +0300

    small corr

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinxcontrib.bibtex',
+    'sphinxcontrib.bibtex'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
